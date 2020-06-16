f42f1062f09bae00d8d88577ffb540a557746f70
Nero <nero@192.168.80.129>
2020-06-16 21:14:32 +0530

    initial xcode project creation

---
+++ Youtube-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-iOS
+//
+//  Created by Nero on 16/06/20.
+//  Copyright © 2020 Nero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
